9dffd9354a8b68df8e41b49466a9b3f537b7b836
sondahum <sondahum@gmail.com>
2020-01-16 14:48:54 +0900

    fix tyop

--- mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDao.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDao.groovy
@@ -5,8 +5,8 @@ import com.sondahum.mamas.elasticsearch.model.SearchOption;
 
 interface EsDao {
 
-    void save(EsDto indexingData) // Create
-    void delete(EsDto indexingData) // Delete
-    void update(EsDto indexingData) // Update
+    void save(EsDto esDto) // Create
+    void delete(EsDto esDto) // Delete
+    void update(EsDto esDto) // Update
     String search(SearchOption searchOption) // Retrieve
 }
