d0ab720e3b55ff3163aa57bd8abc49ea5bb5273a
sondahum <sondahum@gmail.com>
2020-01-16 17:58:11 +0900

    implement C.U.D methods

--- mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDao.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDao.groovy
@@ -5,8 +5,8 @@ import com.sondahum.mamas.elasticsearch.model.SearchOption;
 
 interface EsDao {
 
-    void save(EsDto esDto) // Create
-    void delete(EsDto esDto) // Delete
-    void update(EsDto esDto) // Update
+    void save(List<EsDto> esDto) // Create
+    void delete(List<EsDto> esDto) // Delete
+    void update(List<EsDto> esDto) // Update
     String search(SearchOption searchOption) // Retrieve
 }
