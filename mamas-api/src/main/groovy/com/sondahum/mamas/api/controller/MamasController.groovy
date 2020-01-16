84589ba7f34f9d49235f96af59616e3b06680f22
sondahum <sondahum@gmail.com>
2020-01-16 14:05:42 +0900

    add index method in controller

--- mamas-api/src/main/groovy/com/sondahum/mamas/api/controller/MamasController.groovy
+++ mamas-api/src/main/groovy/com/sondahum/mamas/api/controller/MamasController.groovy
@@ -3,6 +3,7 @@ package com.sondahum.mamas.api.controller
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -10,5 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 class MamasController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName())
+
+    @GetMapping('')
+    void index() {
+
+    }
 
 }
