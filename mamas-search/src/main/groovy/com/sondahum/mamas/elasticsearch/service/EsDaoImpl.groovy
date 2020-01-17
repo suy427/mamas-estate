4350e3a347f664b98570a5693d74c5283a627418
sondahum <sondahum@gmail.com>
2020-01-17 16:20:12 +0900

    add template files

--- mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDaoImpl.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDaoImpl.groovy
@@ -10,22 +10,28 @@ import org.codehaus.jackson.map.ObjectMapper
 import org.elasticsearch.action.bulk.BulkResponse
 import org.elasticsearch.action.delete.DeleteRequest
 import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.action.search.SearchResponse
+import org.elasticsearch.action.search.SearchType
 import org.elasticsearch.action.update.UpdateRequest
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.common.unit.TimeValue
 import org.elasticsearch.common.xcontent.XContentType
+import org.elasticsearch.script.ScriptType
+import org.elasticsearch.script.mustache.SearchTemplateRequest
+import org.elasticsearch.script.mustache.SearchTemplateResponse
+import org.elasticsearch.search.SearchHits
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-
 import javax.annotation.PostConstruct
 
 @Service
 class EsDaoImpl extends RestHighLevelClientHelper implements EsDao {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName())
-    
+
     private ObjectMapper objectMapper
     private BulkProcessor bulkProcessor
     private RestHighLevelClient client
@@ -70,7 +76,7 @@ class EsDaoImpl extends RestHighLevelClientHelper implements EsDao {
 
     @Override
     void save(List<EsDto> esDtoList) {
-        esDtoList.each {dto ->
+        esDtoList.each { dto ->
             byte[] byteContents = objectMapper.writeValueAsBytes(dto)
             bulkProcessor.add(new IndexRequest(dto.indexName).source(byteContents, XContentType.JSON))
         }
@@ -78,20 +84,36 @@ class EsDaoImpl extends RestHighLevelClientHelper implements EsDao {
 
     @Override
     void delete(List<EsDto> esDtoList) {
-        esDtoList.each {dto ->
+        esDtoList.each { dto ->
             bulkProcessor.add(new DeleteRequest(dto.indexName, dto.id))
         }
     }
 
     @Override
     void update(List<EsDto> esDtoList) {
-        esDtoList.each {dto ->
+        esDtoList.each { dto ->
             bulkProcessor.add(new UpdateRequest(dto.indexName, dto.id))
         }
     }
 
     @Override
-    String search(SearchOption searchOption) {
-        return null
+    SearchResponse search(SearchOption searchOption) {
+        SearchTemplateRequest searchTemplateRequest = new SearchTemplateRequest()
+        SearchRequest searchRequest = new SearchRequest(indexInfo.name)
+        searchRequest.searchType(SearchType.DFS_QUERY_THEN_FETCH)
+        searchRequest.allowPartialSearchResults(true)
+
+        searchTemplateRequest.setRequest(searchRequest)
+        searchTemplateRequest.setScriptType(ScriptType.STORED)
+        searchTemplateRequest.setScript('search-template-test1')
+
+        // this is why i make java library. i should make feature that set key of map more flexible
+        Map<String, Object> params = searchOption.toMap()
+        searchTemplateRequest.setScriptParams(params)
+
+        //SearchTemplateResponse VS SearchResponse 알아보기
+        SearchResponse response = client.searchTemplate(searchTemplateRequest, RequestOptions.DEFAULT).getResponse()
+
+        return response
     }
 }
