e3891173dd7a8b4a5eb9e4a1cbc043f2a19226c9
sondahum <sondahum@gmail.com>
2020-01-17 16:20:12 +0900

    add template files

--- mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDao.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/service/EsDao.groovy
@@ -1,12 +1,13 @@
 package com.sondahum.mamas.elasticsearch.service
 
 import com.sondahum.mamas.elasticsearch.model.EsDto
-import com.sondahum.mamas.elasticsearch.model.SearchOption;
+import com.sondahum.mamas.elasticsearch.model.SearchOption
+import org.elasticsearch.action.search.SearchResponse;
 
 interface EsDao {
 
     void save(List<EsDto> esDto) // Create
     void delete(List<EsDto> esDto) // Delete
     void update(List<EsDto> esDto) // Update
-    String search(SearchOption searchOption) // Retrieve
+    SearchResponse search(SearchOption searchOption) // Retrieve
 }
