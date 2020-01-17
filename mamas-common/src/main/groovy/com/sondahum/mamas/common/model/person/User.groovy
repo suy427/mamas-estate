b563a4064e81a354da5ce00e2d3c9ec27989f53a
sondahum <sondahum@gmail.com>
2020-01-17 10:48:42 +0900

    add more fields

--- mamas-common/src/main/groovy/com/sondahum/mamas/common/model/person/User.groovy
+++ mamas-common/src/main/groovy/com/sondahum/mamas/common/model/person/User.groovy
@@ -3,5 +3,4 @@ package com.sondahum.mamas.common.model.person
 class User {
     String name
     String phone
-
 }
--- mamas-common/src/main/groovy/com/sondahum/mamas/common/model/product/Estate.groovy
+++ mamas-common/src/main/groovy/com/sondahum/mamas/common/model/product/Estate.groovy
@@ -1,9 +1,12 @@
 package com.sondahum.mamas.common.model.product
 
+import com.sondahum.mamas.common.model.Address
 import com.sondahum.mamas.common.model.type.ContractType
 import com.sondahum.mamas.common.model.type.EstateType
 
-class Estate {
+class Estate { // 이 땅이 무슨 땅이고,
+    String name
+    Address address
     EstateType productType
     ContractType contractType
 }
