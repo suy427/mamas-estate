c9fb98eb61b1f736ea7399ebd88dc1abc6eb69fd
sondahum <sondahum@gmail.com>
2020-01-20 16:57:18 +0900

    add dtos

---
+++ mamas-search/src/main/groovy/com/sondahum/mamas/elasticsearch/dto/RequestDto.groovy
@@ -0,0 +1,9 @@
+package com.sondahum.mamas.elasticsearch.dto
+
+import com.sondahum.mamas.common.model.person.Client
+import com.sondahum.mamas.elasticsearch.model.SearchOption
+
+class RequestDto implements EsDto {
+
+    private SearchOption searchOption
+}
