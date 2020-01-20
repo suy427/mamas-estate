23e67f20b16301f40b679130fe423ca6f2b2c322
sondahum <sondahum@gmail.com>
2020-01-20 14:07:28 +0900

    using esDaoService in the controllers

--- mamas-api/src/main/groovy/com/sondahum/mamas/api/controller/MamasClientSearchController.groovy
+++ mamas-api/src/main/groovy/com/sondahum/mamas/api/controller/MamasClientSearchController.groovy
@@ -1,7 +1,9 @@
 package com.sondahum.mamas.api.controller
 
+import com.sondahum.mamas.elasticsearch.service.EsDao
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,24 +14,28 @@ class MamasClientSearchController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName())
 
+    @Autowired
+    EsDao esDao
+
+
     @GetMapping('register')
     void registerClient() {
-
+        esDao.save()
     }
 
     @GetMapping('delete')
     void deleteClient() {
-
+        esDao.delete()
     }
 
     @GetMapping('update')
     void updateClient() {
-
+        esDao.update()
     }
 
     @GetMapping('search')
     void searchClient() {
-
+        esDao.search()
     }
 
 }
