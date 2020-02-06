51e7d72f4e88b82566f0d946421574d9397dd43b
suy427 <sondahum@gmail.com>
2020-02-06 14:24:45 +0900

    add @Repository annotation to repositories

--- mamas-search/src/main/groovy/com/sondahum/mamas/repository/UserRepository.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/repository/UserRepository.groovy
@@ -2,7 +2,9 @@ package com.sondahum.mamas.repository
 
 import com.sondahum.mamas.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository  extends JpaRepository<User, Long> {
 
 }
