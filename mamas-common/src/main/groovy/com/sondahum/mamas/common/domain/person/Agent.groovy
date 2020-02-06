a06856086fea067272fab8716a1d2db5b73293e9
suy427 <sondahum@gmail.com>
2020-02-06 15:55:51 +0900

    remove legacy Models

--- mamas-common/src/main/groovy/com/sondahum/mamas/common/domain/person/Agent.groovy
+++
@@ -1,5 +0,0 @@
-package com.sondahum.mamas.common.domain.person
-
-class Agent extends User {
-    String company
-}
--- mamas-common/src/main/groovy/com/sondahum/mamas/common/domain/person/User.groovy
+++
@@ -1,8 +0,0 @@
-package com.sondahum.mamas.common.domain.person
-
-
-class User {
-    String userId
-    String name
-    String phone
-}
