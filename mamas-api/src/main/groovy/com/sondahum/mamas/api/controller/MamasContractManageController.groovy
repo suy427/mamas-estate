9a7717c73b941ab30b23b12b977ede659109aec2
suy427 <sondahum@gmail.com>
2020-02-06 14:21:27 +0900

    split controllers based repository

--- mamas-api/src/main/groovy/com/sondahum/mamas/api/controller/MamasContractManageController.groovy
+++ mamas-api/src/main/groovy/com/sondahum/mamas/api/controller/MamasContractManageController.groovy
@@ -1,5 +1,7 @@
 package com.sondahum.mamas.api.controller
 
+import com.sondahum.mamas.repository.BidRepository
+import com.sondahum.mamas.repository.ContractRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping('/')
-class MamasController {
+class MamasContractManageController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName())
+    private final BidRepository bidRepository
+    private final ContractRepository contractRepository
 
-    MamasController() {
-
+    MamasContractManageController(BidRepository bidRepository, ContractRepository contractRepository) {
+        this.bidRepository = bidRepository
+        this.contractRepository = contractRepository
     }
 
     @GetMapping('')
