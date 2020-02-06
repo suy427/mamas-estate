b3fec10c66103a65dde7f48ff06a3c13ab9914ea
suy427 <sondahum@gmail.com>
2020-02-06 14:24:45 +0900

    add @Repository annotation to repositories

--- mamas-search/src/main/groovy/com/sondahum/mamas/repository/BidRepository.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/repository/BidRepository.groovy
@@ -2,6 +2,8 @@ package com.sondahum.mamas.repository
 
 import com.sondahum.mamas.domain.Bid
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface BidRepository extends JpaRepository<Bid, Long> {
 }
--- mamas-search/src/main/groovy/com/sondahum/mamas/repository/EstateRepository.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/repository/EstateRepository.groovy
@@ -2,7 +2,9 @@ package com.sondahum.mamas.repository
 
 import com.sondahum.mamas.domain.Estate
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface EstateRepository  extends JpaRepository<Estate, Long> {
 
 }
