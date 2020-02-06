68bae094f49aa1372b83d7db98ffc1a15fa846c1
suy427 <sondahum@gmail.com>
2020-02-06 10:56:06 +0900

    add contract entity

---
+++ mamas-search/src/main/groovy/com/sondahum/mamas/domain/Contract.groovy
@@ -0,0 +1,20 @@
+package com.sondahum.mamas.domain
+
+import com.sondahum.mamas.model.NamedEntity
+
+import javax.persistence.AttributeOverride
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "contract")
+@AttributeOverride(name = "id", column = @Column(name = "contract_id"))
+@AttributeOverride(name = "name", column = @Column(name = "contract_name"))
+@AttributeOverride(name = "createdDate", column = @Column(name = "contracted_date"))
+class Contract extends NamedEntity {
+
+    User seller
+    User buyer
+    String price
+}
