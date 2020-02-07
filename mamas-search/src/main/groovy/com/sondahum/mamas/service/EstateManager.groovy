fb2cbad95f6046cf66f0ce16a897a0e084355e40
suy427 <sondahum@gmail.com>
2020-02-07 17:52:35 +0900

    inject dependency of repository into services. not to controllers

--- mamas-search/src/main/groovy/com/sondahum/mamas/service/EstateManager.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/service/EstateManager.groovy
@@ -1,4 +1,17 @@
 package com.sondahum.mamas.service
 
-class EstateManager implements EstateManageService{
+import com.sondahum.mamas.repository.EstateRepository
+import com.sondahum.mamas.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class EstateManager implements EstateManageService {
+
+    private final EstateRepository estateRepository
+    private final UserRepository userRepository
+
+    EstateManager(EstateRepository estateRepository, UserRepository userRepository) {
+        this.estateRepository = estateRepository
+        this.userRepository = userRepository
+    }
 }
