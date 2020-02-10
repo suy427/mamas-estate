13d403aa3d3cc2ea855068dc6577189184f10a24
suy427 <sondahum@gmail.com>
2020-02-10 14:24:41 +0900

    add values of role model

--- mamas-search/src/main/groovy/com/sondahum/mamas/model/Role.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/model/Role.groovy
@@ -1,5 +1,16 @@
 package com.sondahum.mamas.model
 
 enum Role {
-    MAMA, AGENT, OTHER
+    MAMA(1,"MAMA"),
+    AGENT(2, "AGENT"),
+    OTHER(3, "OTHER")
+
+
+    private final int value
+    private final String name
+
+    Role(int value, String name) {
+        this.value = value
+        this.name = name
+    }
 }
