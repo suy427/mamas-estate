230ff24d5ea6e4bcef9583fff1c99a2445777541
suy427 <sondahum@gmail.com>
2020-02-10 14:25:38 +0900

    implement some crud methods

--- mamas-search/src/main/groovy/com/sondahum/mamas/service/CustomerManager.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/service/CustomerManager.groovy
@@ -1,5 +1,9 @@
 package com.sondahum.mamas.service
 
+import com.sondahum.mamas.domain.Bid
+import com.sondahum.mamas.domain.User
+import com.sondahum.mamas.dto.BidDto
+import com.sondahum.mamas.dto.UserDto
 import com.sondahum.mamas.repository.BidRepository
 import com.sondahum.mamas.repository.UserRepository
 import org.springframework.stereotype.Service
@@ -14,4 +18,26 @@ class CustomerManager implements CustomerManageService {
         this.userRepository = userRepository
         this.bidRepository = bidRepository
     }
+
+    List<UserDto> getUserList() {
+        List<User> userEntityList = userRepository.findAll()
+        List<UserDto> userDtoList = userEntityList.collect { userEntity ->
+            userEntity.toDto()
+        }
+        return userDtoList
+    }
+
+    String createNewUser(UserDto userDto) {
+        User userEntity = userDto.toEntity()
+        userRepository.save(userEntity)
+    }
+
+    String createNewBid(BidDto bidDto) { // with user info upsert.
+        Bid bidEntity = bidDto.toEntity()
+        bidRepository.save(bidEntity)
+    }
+
+
+
+
 }
