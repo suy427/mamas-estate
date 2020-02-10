3c50e57a6e3b3b435f1874e6d0cc9642bad83417
suy427 <sondahum@gmail.com>
2020-02-10 18:50:29 +0900

    initial commit for mamas-config

---
+++ mamas-config/src/main/groovy/com/sondahum/mamas/tester/MockTestRequestHandler.groovy
@@ -0,0 +1,22 @@
+package com.sondahum.mamas.tester;
+
+
+import org.junit.runner.RunWith
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MamasTestStarter.class)
+@AutoConfigureMockMvc
+abstract class MockTestRequestHandler {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    MockMvc mvc
+}
