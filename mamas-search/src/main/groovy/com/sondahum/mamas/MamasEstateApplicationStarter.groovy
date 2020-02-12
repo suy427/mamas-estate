0dfb8c12d8971f4beafbf819db58488f0a56db94
suy427 <sondahum@gmail.com>
2020-02-12 17:12:10 +0900

    module structure stabilizing

--- mamas-search/src/main/groovy/com/sondahum/mamas/MamasEstateApplicationStarter.groovy
+++ mamas-search/src/main/groovy/com/sondahum/mamas/MamasEstateApplicationStarter.groovy
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class SearchStarter extends SpringBootServletInitializer {
+class MamasEstateApplicationStarter extends SpringBootServletInitializer {
 
     static void main(String[] args) { // 이거야 뭐 이 클래스를 SpringBoot의 Starter로 실행시키겠다는거겠고
-        SpringApplication.run(SearchStarter, args)
+        SpringApplication.run(MamasEstateApplicationStarter, args)
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { //이게 뭔지 공부하자
-        application.sources(SearchStarter)
+        application.sources(MamasEstateApplicationStarter)
     }
 }
