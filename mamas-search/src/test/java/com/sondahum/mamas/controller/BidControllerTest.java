9bacf94b0035b6dafa7f9313d2be95d9e1b77c69
suy427 <sondahum@gmail.com>
2020-03-06 17:33:26 +0900

    add controller test

---
+++ mamas-search/src/test/java/com/sondahum/mamas/controller/BidControllerTest.java
@@ -0,0 +1,13 @@
+package com.sondahum.mamas.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class BidControllerTest {
+
+    
+
+}
---
+++ mamas-search/src/test/java/com/sondahum/mamas/controller/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.sondahum.mamas.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+}
