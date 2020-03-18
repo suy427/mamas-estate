ecbf8ec15f01e94cc23441a010e959293a9c568b
suy427 <sondahum@gmail.com>
2020-03-19 00:25:26 +0900

    a

--- mamas-search/src/test/java/com/sondahum/mamas/AbstractTestHelper.java
+++ mamas-search/src/test/java/com/sondahum/mamas/AbstractTestHelper.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
