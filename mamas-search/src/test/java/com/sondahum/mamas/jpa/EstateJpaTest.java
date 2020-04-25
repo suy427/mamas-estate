ee3fd28f6d909a677963329ed9aea74f0e756bf4
sondahum <sondahum@gmail.com>
2020-04-26 02:59:06 +0900

    rename repository test --> jpa test

--- mamas-search/src/test/java/com/sondahum/mamas/jpa/EstateJpaTest.java
+++ mamas-search/src/test/java/com/sondahum/mamas/jpa/EstateJpaTest.java
@@ -1,58 +1,58 @@
-package com.sondahum.mamas.jpa;
-
-
-import com.sondahum.mamas.domain.estate.Estate;
-import com.sondahum.mamas.domain.estate.EstateRepository;
-import com.sondahum.mamas.domain.user.User;
-import com.sondahum.mamas.domain.user.UserRepository;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static com.sondahum.mamas.testutil.TestValueGenerator.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@DataJpaTest
-@RunWith(SpringRunner.class)
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class EstateJpaTest {
-
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    EstateRepository estateRepository;
-
+//package com.sondahum.mamas.jpa;
+//
+//
+//import com.sondahum.mamas.domain.estate.Estate;
+//import com.sondahum.mamas.domain.estate.EstateRepository;
+//import com.sondahum.mamas.domain.user.User;
+//import com.sondahum.mamas.domain.user.UserRepository;
+//import org.hamcrest.CoreMatchers;
+//import org.hamcrest.MatcherAssert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static com.sondahum.mamas.testutil.TestValueGenerator.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@DataJpaTest
+//@RunWith(SpringRunner.class)
+//public class EstateJpaTest {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//    @Autowired
+//    EstateRepository estateRepository;
+//
 //    @Test
-    void createEstateTest() {
-        List<User> initialEstateOwners = new ArrayList<>();
-        List<Estate> initialEstates = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            User user = userInfoGenerator();
-            Estate estate = estateInfoGenerator(user);
-            initialEstateOwners.add(user);
-            initialEstates.add(estate);
-        }
-        estateRepository.saveAll(initialEstates);
-
-        List<Estate> savedEstates = estateRepository.findAll();
-
-        for (int i = 0; i < 10; i++) {
-            Estate initial = initialEstates.get(i);
-            Estate saved = savedEstates.get(i);
-
-            MatcherAssert.assertThat(initial.getAddress(), CoreMatchers.is(saved.getAddress()));
-            MatcherAssert.assertThat(initial.getArea(), CoreMatchers.is(saved.getArea()));
-            MatcherAssert.assertThat(initial.getEstateType(), CoreMatchers.is(saved.getEstateType()));
-            MatcherAssert.assertThat(initial.getContractType(), CoreMatchers.is(saved.getContractType()));
-            MatcherAssert.assertThat(initial.getMarketPriceRange(), CoreMatchers.is(saved.getMarketPriceRange()));
-            MatcherAssert.assertThat(initial.getOwnerRequirePriceRange(), CoreMatchers.is(saved.getOwnerRequirePriceRange()));
-            MatcherAssert.assertThat(initial.getOwner(), CoreMatchers.is(saved.getOwner()));
-        }
-        System.out.println("break point");
-    }
-}
+//    void createEstateTest() {
+//        List<User> initialEstateOwners = new ArrayList<>();
+//        List<Estate> initialEstates = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            User user = userInfoGenerator();
+//            Estate estate = estateInfoGenerator(user);
+//            initialEstateOwners.add(user);
+//            initialEstates.add(estate);
+//        }
+//        estateRepository.saveAll(initialEstates);
+//
+//        List<Estate> savedEstates = estateRepository.findAll();
+//
+//        for (int i = 0; i < 10; i++) {
+//            Estate initial = initialEstates.get(i);
+//            Estate saved = savedEstates.get(i);
+//
+//            MatcherAssert.assertThat(initial.getAddress(), CoreMatchers.is(saved.getAddress()));
+//            MatcherAssert.assertThat(initial.getArea(), CoreMatchers.is(saved.getArea()));
+//            MatcherAssert.assertThat(initial.getEstateType(), CoreMatchers.is(saved.getEstateType()));
+//            MatcherAssert.assertThat(initial.getContractType(), CoreMatchers.is(saved.getContractType()));
+//            MatcherAssert.assertThat(initial.getMarketPriceRange(), CoreMatchers.is(saved.getMarketPriceRange()));
+//            MatcherAssert.assertThat(initial.getOwnerRequirePriceRange(), CoreMatchers.is(saved.getOwnerRequirePriceRange()));
+//            MatcherAssert.assertThat(initial.getOwner(), CoreMatchers.is(saved.getOwner()));
+//        }
+//        System.out.println("break point");
+//    }
+//}
