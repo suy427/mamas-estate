cf13d542884f1604edfc1d8308b47e7ce1ab6b6c
sondahum <sondahum@gmail.com>
2020-04-26 03:03:00 +0900

    add user create test at controller unit test

--- mamas-search/src/test/java/com/sondahum/mamas/controller/MockUserControllerTest.java
+++ mamas-search/src/test/java/com/sondahum/mamas/controller/MockUserControllerTest.java
@@ -1,28 +1,100 @@
 package com.sondahum.mamas.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sondahum.mamas.MamasEstateApplicationStarter;
 import com.sondahum.mamas.domain.user.UserInfoService;
 import com.sondahum.mamas.domain.user.UserSearchService;
+import com.sondahum.mamas.domain.user.model.Role;
+import com.sondahum.mamas.dto.UserDto;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+@AutoConfigureMockMvc
 @RunWith(MockitoJUnitRunner.class)
 public class MockUserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @InjectMocks
+    private UserController userController;
     @Mock
     private UserInfoService infoService;
     @Mock
     private UserSearchService searchService;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private MockMvc mockMvc;
+
     @Before
     public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController)
+//                .setControllerAdvice(new ErrorController())
+                .build();
+    }
 
+    @Test
+    public void 유저생성() throws Exception {
+        //given
+        final UserDto.CreateReq dto = UserDto.CreateReq.builder()
+                .name("김철수")
+                .phone("101-0011-1101")
+                .role("OTHER")
+                .build();
+        given(infoService.createUserInfo(any())).willReturn(dto.toEntity());
+
+        //when
+        final MockHttpServletResponse resultActions = requestCreateUser(dto).andReturn().getResponse();
+
+        MatcherAssert.assertThat(resultActions.getStatus(), CoreMatchers.is(200));
+    }
+
+    private ResultActions requestCreateUser(UserDto.CreateReq dto) throws Exception {
+        return mockMvc.perform(
+                post("/user")
+                        .content(objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    private ResultActions requestUpdateUser(UserDto.UpdateReq dto) throws Exception {
+        return mockMvc.perform(
+                put("/user")
+                        .content(objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    private ResultActions requestGetUser() throws Exception {
+        return mockMvc.perform(get("/user/" + anyLong())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 }
