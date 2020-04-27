e83ad54215cf02ba8fde01904572e998cc30824d
sonjongyoung <sondahum@gmail.com>
2020-04-27 13:52:12 +0900

    fix typo

--- mamas-search/src/test/java/com/sondahum/mamas/jpa/BidJpaTest.java
+++ mamas-search/src/test/java/com/sondahum/mamas/jpa/BidJpaTest.java
@@ -1,35 +1,35 @@
-package com.sondahum.mamas.jpa;
-
-import com.sondahum.mamas.domain.bid.model.Action;
-import com.sondahum.mamas.domain.bid.Bid;
-import com.sondahum.mamas.domain.bid.BidRepository;
-import com.sondahum.mamas.domain.estate.Estate;
-import com.sondahum.mamas.domain.estate.EstateRepository;
-import com.sondahum.mamas.domain.user.User;
-import com.sondahum.mamas.domain.user.UserRepository;
-import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-
-@DataJpaTest
-@RunWith(SpringRunner.class)
-public class BidJpaTest {
-
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    BidRepository bidRepository;
-    @Autowired
-    EstateRepository estateRepository;
-
-
-    @Test
-    @Ignore
-    void bidInfoCascadeTest() {
-    }
-
-}
+//package com.sondahum.mamas.jpa;
+//
+//import com.sondahum.mamas.domain.bid.model.Action;
+//import com.sondahum.mamas.domain.bid.Bid;
+//import com.sondahum.mamas.domain.bid.BidRepository;
+//import com.sondahum.mamas.domain.estate.Estate;
+//import com.sondahum.mamas.domain.estate.EstateRepository;
+//import com.sondahum.mamas.domain.user.User;
+//import com.sondahum.mamas.domain.user.UserRepository;
+//import org.junit.Ignore;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//
+//@DataJpaTest
+//@RunWith(SpringRunner.class)
+//public class BidJpaTest {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//    @Autowired
+//    BidRepository bidRepository;
+//    @Autowired
+//    EstateRepository estateRepository;
+//
+//
+//    @Test
+//    @Ignore
+//    void bidInfoCascadeTest() {
+//    }
+//
+//}
