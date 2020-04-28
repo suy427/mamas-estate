0df8f1480c904eed2a2b9a51447c4921e3102402
sonjongyoung <sondahum@gmail.com>
2020-04-28 18:04:37 +0900

    add jpa tests

--- mamas-search/src/test/java/com/sondahum/mamas/jpa/BidJpaTest.java
+++ mamas-search/src/test/java/com/sondahum/mamas/jpa/BidJpaTest.java
@@ -1,35 +1,65 @@
-//package com.sondahum.mamas.jpa;
-//
-//import com.sondahum.mamas.domain.bid.model.Action;
-//import com.sondahum.mamas.domain.bid.Bid;
-//import com.sondahum.mamas.domain.bid.BidRepository;
-//import com.sondahum.mamas.domain.estate.Estate;
-//import com.sondahum.mamas.domain.estate.EstateRepository;
-//import com.sondahum.mamas.domain.user.User;
-//import com.sondahum.mamas.domain.user.UserRepository;
-//import org.junit.Ignore;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//
-//@DataJpaTest
-//@RunWith(SpringRunner.class)
-//public class BidJpaTest {
-//
-//    @Autowired
-//    UserRepository userRepository;
-//    @Autowired
-//    BidRepository bidRepository;
-//    @Autowired
-//    EstateRepository estateRepository;
-//
-//
-//    @Test
-//    @Ignore
-//    void bidInfoCascadeTest() {
-//    }
-//
-//}
+package com.sondahum.mamas.jpa;
+
+import com.sondahum.mamas.domain.bid.Bid;
+import com.sondahum.mamas.domain.bid.BidRepository;
+import com.sondahum.mamas.domain.estate.Estate;
+import com.sondahum.mamas.domain.estate.EstateRepository;
+import com.sondahum.mamas.domain.user.User;
+import com.sondahum.mamas.domain.user.UserRepository;
+import com.sondahum.mamas.testutil.TestValueGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+
+@DataJpaTest
+@Slf4j
+public class BidJpaTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BidRepository bidRepository;
+    @Autowired
+    EstateRepository estateRepository;
+
+
+    @Test
+    void 호가정보_생성() {
+        User user1 = TestValueGenerator.userInfoGenerator();
+        Estate estate1 = TestValueGenerator.estateInfoGenerator(user1);
+
+        Bid bid1 = TestValueGenerator.bidInfoGenerator(user1, estate1);
+        Bid result = bidRepository.save(bid1);
+
+        MatcherAssert.assertThat(result, CoreMatchers.is(bid1));
+    }
+
+    @Test
+    void 유저_호가_컬렉션_추가() {
+        User user1 = TestValueGenerator.userInfoGenerator();
+        Estate estate1 = TestValueGenerator.estateInfoGenerator(user1);
+
+        User savedUser1 = userRepository.save(user1);
+        Estate savedEstate1 = estateRepository.save(estate1);
+
+        Bid bid1 = TestValueGenerator.bidInfoGenerator(savedUser1, savedEstate1);
+
+        Bid savedBid1 = bidRepository.save(bid1);
+
+        MatcherAssert.assertThat(savedUser1, CoreMatchers.is(user1));
+
+
+        user1.getBidList().add(bid1);
+
+
+    }
+
+}
--- mamas-search/src/test/java/com/sondahum/mamas/jpa/ContractJpaTest.java
+++ mamas-search/src/test/java/com/sondahum/mamas/jpa/ContractJpaTest.java
@@ -5,7 +5,6 @@
 //import org.springframework.test.context.junit4.SpringRunner;
 //
 //@DataJpaTest
-//@RunWith(SpringRunner.class)
 //public class ContractJpaTest {
 //
 //}
